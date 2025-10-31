c7974007d914f7baf0295df0a8b9e4b39eb6d8c0
Gambhiro <gambhiro@users.noreply.github.com>
2025-10-31 16:51:55 +0100

    checks

---
+++ cli/xml-parsing-checks.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [ -f fragments.sqlite3 ]; then
+    rm fragments.sqlite3
+fi
+
+if [ -f suttas.sqlite3 ]; then
+    rm suttas.sqlite3
+fi
+
+cargo run -- parse-tipitaka-xml ../../bootstrap-assets-resources/tipitaka-org-vri-cst/tipitaka-xml/romn/s0101m.mul.xml suttas.sqlite3 --fragments-db fragments.sqlite3
+cargo run -- parse-tipitaka-xml ../../bootstrap-assets-resources/tipitaka-org-vri-cst/tipitaka-xml/romn/s0201m.mul.xml suttas.sqlite3 --fragments-db fragments.sqlite3
+
+cargo run -- reconstruct-xml-from-fragments ./fragments.sqlite3 s0101m.mul.xml ./s0101m.mul.xml
+cargo run -- reconstruct-xml-from-fragments ./fragments.sqlite3 s0201m.mul.xml ./s0201m.mul.xml
+
+sed -i 's/UTF-16/UTF-8/' ./*.xml
+
+echo "Diff files:"
+
+diff ./tests/data/s0101m.mul.xml ./s0101m.mul.xml
+diff ./tests/data/s0201m.mul.xml ./s0201m.mul.xml
+
+echo "Done"
